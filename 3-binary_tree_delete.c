b60cb7a0b2a0de57c01164721eec7d80807d2b33
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-02-27 20:39:42 +0100

    Update 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,1 +1,14 @@
-qwertyu
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: tree to delete
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
+}
