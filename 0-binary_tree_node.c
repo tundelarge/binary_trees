877c8ada625262095fbb064d6d20ae49cf296904
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-02-28 20:59:50 +0100

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,25 +1,35 @@
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * main - Entry point
+ * binary_tree_node - function that creates a binary tree node
+ * @parent: a pointer to the parent node of the node to create
+ * @value: the value to put in the new node
  *
- * Return: Always 0 (Success)
+ * Return: a pointer to the new node, or NULL on failure
+ *
+ * FUNCTIONALITY
+ *
+ * 1. Allocate memory for a new binary tree node.
+ * 2. If the allocation fails, return NULL.
+ * 3. Set the value of the new node to the value passed in.
+ * 4. Set the parent of the new node to the parent passed in.
+ * 5. Set the left child of the new node to NULL.
+ * 6. Set the right child of the new node to NULL.
+ * 7. Return a pointer to the new node.
+ *
+ * Time Complexity: O(1)
  */
-int main(void)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *root;
+	binary_tree_t *new;
 
-    root = binary_tree_node(NULL, 98);
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
 
-    root->left = binary_tree_node(root, 12);
-    root->left->left = binary_tree_node(root->left, 6);
-    root->left->right = binary_tree_node(root->left, 16);
-
-    root->right = binary_tree_node(root, 402);
-    root->right->left = binary_tree_node(root->right, 256);
-    root->right->right = binary_tree_node(root->right, 512);
-
-    binary_tree_print(root);
-    return (0);
+	return (new);
 }
