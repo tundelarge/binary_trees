0a2e8a8bea19db4fac329c7199551c8424357513
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-03-02 22:28:04 +0100

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -26,7 +26,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
  *         NULL if the parent is NULL
  *         NULL if the node has no siblings
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *nod)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
