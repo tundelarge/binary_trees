f31438d2aafd3f54c050fbc2268e663fb9bb1291
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-03-02 22:33:37 +0100

    Update 6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,5 +12,5 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, fun);
+	binary_tree_preorder(tree->right, func);
 }
