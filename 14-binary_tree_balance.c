2c6a6f4c9d30c7e78102de527e7d1a3f188b703b
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-03-02 22:06:28 +0100

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,7 +32,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_l = 0;
 	size_t height_r = 0;
 
-	if (!tree)
+	if (!tre)
 		return (0);
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
