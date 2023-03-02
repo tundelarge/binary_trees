8079eb50ba50f904d685cf27e49b0c65e4394d78
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-03-02 22:05:11 +0100

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -32,7 +32,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 /**
  * binary_tree_leaves - counts the leaves in a binary tree
- * @tree: tree to count the leaves from
+ * @tree: tree to count the leaves fro
  *
  * Return: number of leaves
  *         0 if tree is NULL
