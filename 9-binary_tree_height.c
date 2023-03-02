48ebb9a34232a5e0da6a3ebb74128d267ecff60d
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-03-02 22:38:33 +0100

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,5 +17,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	retur (height_l > height_r ? height_l : height_r);
+	return (height_l > height_r ? height_l : height_r);
 }
