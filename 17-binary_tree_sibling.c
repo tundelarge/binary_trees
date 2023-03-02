e73754895f5527d6342f4eaf1a39e3e33668f60c
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-03-02 22:26:28 +0100

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -16,5 +16,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	if (node == node->parent->left)
 		return (node->parent->right);
-	return (node->parent->left);
+	return (node->parent->lef);
 }
